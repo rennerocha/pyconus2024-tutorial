2764cc5989617fd7d2c11533e9c6534a99be6dc0
Renne Rocha <renne@rennerocha.com>
2024-05-02 19:51:40 -0300

    Code of second example

---
+++ code/pyconus2024.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class PyConUS2024Spider(scrapy.Spider):
+    name = "pyconus"
+
+    start_urls = [
+        'https://us.pycon.org/2024/schedule/tutorials/',
+    ]
+
+    def parse(self, response):
+        for tutorial in response.css('.calendar a::text').getall():
+            yield {"title": tutorial}
