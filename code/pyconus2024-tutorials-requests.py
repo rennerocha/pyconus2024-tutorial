d7fd3dd578901f47577fb2d8f517b9396c45a3b0
Renne Rocha <renne@rennerocha.com>
2024-05-03 15:31:11 -0300

    Fix example code

--- code/pyconus2024-tutorials-requests.py
+++ code/pyconus2024-tutorials-requests.py
@@ -3,6 +3,6 @@ from parsel import Selector
 
 response = requests.get('https://us.pycon.org/2024/schedule/tutorials/')
 
-sel = Selector(text=response.body)
+sel = Selector(text=response.text)
 for tutorial in sel.css('.calendar a::text').getall():
     print(tutorial)
